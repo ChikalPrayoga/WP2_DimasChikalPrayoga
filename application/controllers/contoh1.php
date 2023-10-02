2743cb387a49abe33b3251b1fbcfb72d99c4de08
Dimaschikal <dimaschikalprayoga@gmail.com>
2023-10-02 20:10:10 +0700

    menambahkan file

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalan</h1>";
+    echo "Nama saya Dimas Chikal Prayoga. Saya tinggal di daerah Bekasi.";
+    echo "Olahraga yang saya sukai adalah badminton.";
+  }
+}
