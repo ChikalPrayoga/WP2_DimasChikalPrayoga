846a852c9793fc68c31a0b6db8f5ed56cf10fb66
Dimaschikal <dimaschikalprayoga@gmail.com>
2023-10-04 20:43:44 +0700

    hanya mengganti pesan 'menambahkan controller_latihan1' menjadi menambahkan Model_latihan1

--- application/controllers/Model_latihan1.php
+++ application/controllers/Model_latihan1.php
@@ -3,7 +3,7 @@ class Model_latihan1 extends CI_Model
 {
     //membuat variabel untuk menampung nilai
     public $nilai1, $nilai2, $nilai3;
-    //method penjumlahan
+    //method penjumlahan.
     public function jumlah($n1 = null, $n2 = null)
     {
         $this->nilai1 = $nil1;
