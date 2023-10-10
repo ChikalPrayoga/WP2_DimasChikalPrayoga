08317bd60502d8a3e72f8d5e660adc2b48bbe48d
Dimas Chikal <dimaschikalprayoga@gmail.com>
2023-10-10 20:16:28 +0700

    mengerjakan tugas pertemuan 2

---
+++ application/controllers/matakuliah.php
@@ -0,0 +1,28 @@
+<?php
+class Matakuliah extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+
+    public function cetak()
+    {
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required');
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required');
+        $this->form_validation->set_rules('sks', 'SKS', 'required|numeric');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+            $this->load->view('view-data-matakuliah', $data);
+        }
+    }
+}
